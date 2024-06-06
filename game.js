dd95396226bdd0a269b9372bf4d4c653009de084
aregm83 <aregm83@gmail.com>
2024-06-05 23:23:04 -0230

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,8 @@
 const armenianAlphabet = [
-   'Ա', 'Բ', 'Գ', 'Դ', 'Ե', 'Զ', 'Է', 'Ը', 'Թ', 'Ժ', 'Ի', 'Լ', 'Խ', 'Ծ', 'Կ', 'Հ', 'Ձ', 'Ղ', 'Ճ', 'Մ', 'Յ', 'Ն', 'Շ', 'Ո', 'Չ', 'Պ', 'Ջ', 'Ռ', 'Ս', 'Վ', 'Տ', 'Ր', 'Ց', 'ւ', 'Փ', 'Ք', 'և', 'Օ', 'Ֆ'
+   'Ա', 'Բ', 'Գ', 'Դ', 'Ե', 'Զ', 'Է', 'Ը', 'Թ', 'Ժ', 'Ի', 'Լ', 'Խ', 'Ծ', 'Կ', 'Հ', 'Ձ', 'Ղ', 'Ճ', 'Մ', 'Յ', 'Ն', 'Շ', 'Ո', 'Չ', 'Պ', 'Ջ', 'Ռ', 'Ս', 'Վ', 'Տ', 'Ր', 'Ց', 'Ւ', 'Փ', 'Ք', 'և', 'Օ', 'Ֆ'
 ];
 
-const vowels = new Set(['Ա', 'Ե', 'Է', 'Ը', 'Ի', 'Ո', 'Օ', 'և', 'ւ']); // Set of Armenian vowels
-
-const phoneticKeyboardMap = {
- //'Ա': 'A',   'Բ': 'B',   'Գ': 'G',   'Դ': 'D',   'Ե': 'E',   'Զ': 'Z',   'Է': '1',   'Ը': 'Y',   'Թ': '2',   'Ժ': '=',   'Ի': 'I',   'Լ': 'L',   'Խ': '{',   'Ծ': '}',   'Կ': 'K',   'Հ': 'H',   'Ձ': '4',   'Ղ': 'X',   'Ճ': '0',   'Մ': 'M',   'Յ': 'J',   'Ն': 'N',   'Շ': '\\',   'Ո': 'w',   'Չ': '9',   'Պ': 'P',   'Ջ': '5',   'Ռ': 'R',   'Ս': 'S',   'Վ': 'V',   'Տ': 'T',   'Ր': '8',   'Ց': 'C',   'Փ': '3',   'Ք': 'q',   'Օ': 'O',   'Ֆ': 'F',   'և': '7',   'ւ': 'U' };
- 'Ա': '',   'Բ': '',   'Գ': '',   'Դ': '',   'Ե': '',   'Զ': '',   'Է': '1',   'Ը': 'Y',   'Թ': '2',   'Ժ': '=',   'Ի': '',   'Լ': '',   'Խ': '{',   'Ծ': '}',   'Կ': '',   'Հ': '',   'Ձ': '4',   'Ղ': 'X',   'Ճ': '',   'Մ': '',   'Յ': '',   'Ն': '',   'Շ': '\\',   'Ո': 'w',   'Չ': '9',   'Պ': '',   'Ջ': '5',   'Ռ': '',   'Ս': '',   'Վ': '',   'Տ': '',   'Ր': '8',   'Ց': '',   'Փ': '3',   'Ք': '',   'Օ': '',   'Ֆ': '',   'և': '7',   'ւ': 'U' };
+const vowels = new Set(['Ա', 'Ե', 'Է', 'Ը', 'Ի', 'Ո', 'Օ', 'Ւ', 'և']); // Set of Armenian vowels
 
 const gameContainer = document.getElementById('game');
 const messageDiv = document.getElementById('message');
@@ -20,10 +16,10 @@ let speedMultiplier = 1; // Multiplier to adjust speed based on performance
 let hitCount = 0; // Number of consecutive hits without mistakes
 
 function getUserName() {
-    userName = prompt('Մուտքագրե՛ք Ձեր անունը:');
-    if (!userName) {
+//    userName = prompt('Մուտքագրե՛ք Ձեր անունը:');
+//    if (!userName) {
         userName = 'Հարգելի օգտագործող';
-    }
+//    }
 }
 
 function getRandomArmenianLetter() {
@@ -33,8 +29,7 @@ function getRandomArmenianLetter() {
 function createLetterElement(letter) {
     const element = document.createElement('div');
     element.classList.add('letter');
-    const phoneticSymbol = phoneticKeyboardMap[letter] || '';
-    element.textContent = `${letter} ${letter.toLowerCase()} - ${phoneticSymbol}`;
+    element.textContent = `${letter} ${letter.toLowerCase()}`;
     element.classList.add(vowels.has(letter) ? 'vowel' : 'consonant');
     element.style.left = `${Math.random() * 230}px`; // Random horizontal position within game container
     return element;
@@ -51,7 +46,7 @@ function startGame() {
             position += 2 * speedMultiplier; // Increase speed based on performance
             letterElement.style.top = `${position}px`;
 
-            if (position > 500) { // Letter reached the bottom
+            if (position > 300) { // Letter reached the bottom
                 clearInterval(gameInterval);
                 alert(`${userName}, խաղն ավարտվեց։`);
                 resetGame();
@@ -64,7 +59,7 @@ function startGame() {
 }
 
 function updateSpeedDisplay() {
-    speedDisplay.textContent = `Շարահյուսական արագություն: ${speedMultiplier}x`;
+    speedDisplay.textContent = `Արագություն: ${speedMultiplier}x`;
 }
 
 function resetGame() {
